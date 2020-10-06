2c69cd91789ffed5362236bc0107a17e61f4229e
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-10-06 20:01:15 +0530

    Configured postgres db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 
 
 class CommandTests(TestCase):
-    
+
     def test_wait_for_db_ready(self):
         """Test waiting for Db when db is available"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
